0aa32ae8632d4af33f68e262ea35eb52de7a30b6
David Keyes <david@rfortherestofus.com>
2024-02-27 14:16:20 -0800

    Delete packages

--- functions.R
+++
@@ -1,8 +0,0 @@
-paginated_table <- function(df, page_size = 5) {
-  df %>% 
-    reactable(defaultPageSize = page_size,
-              searchable = TRUE,
-              compact = TRUE,
-              class = "rrutable")
-  
-}
